e544dbbb899e885920823326781a8ccb5af163cd
khalidnt <405795+khalidnt@users.noreply.github.com>
2018-12-11 17:03:58 +0300

    lab @ 5pm

--- controllers/termsController.js
+++ controllers/termsController.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var term = require('../models/term.js');
+
+
+router.get('/', term.getAll, renderIndex );
+// router.get('/:id',);
+function renderIndex(req, res){
+    var mustacheVariables = 
+        res.locals.term;
+     
+     res.render('./terms/index', mustacheVariables);
+   }
+   
 
 
 module.exports = router;
--- models/term.js
+++ models/term.js
@@ -1,5 +1,17 @@
 var db = require('../db/config');
 var term = {};
 
+term.getAll = function(req, res){
+    db.manyOrNone("SELECT * FROM express_terms;")
+    .then(function(result){
+        res.locals.term = result
+    next();
+    })
+    .catch(function(error){ 
+        console.log(error);
+        next(); 
+      })
+}  
+
 
 module.exports = term;
